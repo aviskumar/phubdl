6743be8cbf08b24738ce0b833fbc68ad40a7c079
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-08-02 14:31:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "1907444863:AAG7RGkhgXqAZHgtJ6AtMsTasvpF9L27vEs")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "JHUEUF-BARYKF-GJZOXX-PXOEHV-ARQ")
+TOKEN = os.environ.get("TOKEN", "1941538288:AAFGDdE8zD7UeW_Zn-af5LtkGWH92vNprnM")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "EKBRLS-VQDSNM-ESXMVY-ZNTPEY-ARQ)
 REPO_BOT = os.environ.get("REPO_BOT", "https://telegra.ph/DOCTYPE-htmlmeta-namesatyanand-lovelinux-1-08-02-3")
-OWNER = os.environ.get("OWNER", "YatoHentaiBot")
-BOT_NAME = os.environ.get("BOT_NAME", "Rikudopurnhubdl")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "emcee_updates")
+OWNER = os.environ.get("OWNER", "Avish_Kumar")
+BOT_NAME = os.environ.get("BOT_NAME", "Tharkio")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "telegram")
