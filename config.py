02f65909b3d8d4751d9f0b8e3953c4bf89709219
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2021-08-02 13:24:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/levina-lab/PornHub")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "levinachannel")
+TOKEN = os.environ.get("TOKEN", "1907444863:AAG7RGkhgXqAZHgtJ6AtMsTasvpF9L27vEs")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "JHUEUF-BARYKF-GJZOXX-PXOEHV-ARQ")
+REPO_BOT = os.environ.get("REPO_BOT", "https://telegra.ph/DOCTYPE-htmlmeta-namesatyanand-lovelinux-1-08-02-3")
+OWNER = os.environ.get("OWNER", "YatoHentaiBot")
+BOT_NAME = os.environ.get("BOT_NAME", "Rikudopurnhubdl")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "emcee-updates")
