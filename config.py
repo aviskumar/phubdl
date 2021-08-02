65cfb210791c54a90b7bd9aa80791b0981539762
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-08-02 14:47:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "1941538288:AAFGDdE8zD7UeW_Zn-af5LtkGWH92vNprnM")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "EKBRLS-VQDSNM-ESXMVY-ZNTPEY-ARQ)
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "EKBRLS-VQDSNM-ESXMVY-ZNTPEY)
 REPO_BOT = os.environ.get("REPO_BOT", "https://telegra.ph/DOCTYPE-htmlmeta-namesatyanand-lovelinux-1-08-02-3")
 OWNER = os.environ.get("OWNER", "Avish_Kumar")
 BOT_NAME = os.environ.get("BOT_NAME", "Tharkio")
